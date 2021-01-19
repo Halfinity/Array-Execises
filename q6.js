08ac4cf16f34dc2754428528bb7762666f30bd43
Halfinity <ofirhalfin13@gmail.com>
2021-01-19 12:56:48 +0200

    q6 fix

--- q6.js
+++ q6.js
@@ -1,15 +1,8 @@
-function insertHyphen(str) {
-    var strArr = str.split('');
-    var numArr = strArr.map(Number);
-    var result ="";
-    for(var i = 0; i < numArr.length; i++) {
-     if((numArr[i+1]!==undefined)&&(numArr[i]%2===0 && numArr[i+1]%2===0)) {
-     
-          result = result + numArr[i] + "-";
-      }else{
-          result = result + numArr[i];
-      }
-    }
-    return result;
-  }
-  console.log(insertHyphen('112233445566'));
+var numbers = '';
+for (var i = 0; i < 20; i++) {
+  numbers += Math.floor(Math.random() * 10);
+}
+
+numbers = numbers.replace(/([02468])(?=[02468])/g, '$1-');
+
+console.log(numbers);
